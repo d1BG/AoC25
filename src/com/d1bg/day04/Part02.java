1f889ec6c8d5deac35785d7287ea4ab32949af75
Denis Georgiev <d1bg@tuta.io>
2025-12-04 14:22:24 +0200

    better use of OOP stuff

--- src/com/d1bg/day04/Part02.java
+++ src/com/d1bg/day04/Part02.java
@@ -1,20 +1,17 @@
 package com.d1bg.day04;
 
-public class Part02 {
-    char[][] grid;
+public class Part02 extends Part01 {
     int paperCollected = 0;
     int paperCollectedLastRun = 0;
 
     public Part02(char[][] grid, boolean part2) {
-        this.grid = grid;
+        super(grid, part2);
     }
 
     public int part2Solution() {
-        Part01 part2 = new Part01(grid, true);
-
         do {
             paperCollectedLastRun = paperCollected;
-            paperCollected += part2.part1Solution();
+            paperCollected += part1Solution();
         } while (paperCollected > paperCollectedLastRun);
 
         return paperCollected;
